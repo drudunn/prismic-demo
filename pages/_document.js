8019413e6d2b3e8bd78c973886a96560a1ca9dc3
Andru Dunn <andru@dunn.wtf>
2020-10-13 23:04:53 +1100

    add preview/edit toolbar script

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const repoName = process.env.PRISMIC_REPOSITORY_NAME
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async defer src={`//static.cdn.prismic.io/prismic.js?repo=${repoName}&new=true`} />
+        </Head>
         <body>
           <Main />
           <NextScript />
